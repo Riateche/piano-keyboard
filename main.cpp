4846da4985b75a3dc10c16d614ed914f8e354410
Vadim Kaushan <admin@disasm.info>
2016-01-01 12:59:04 +0300

    get device name from the command line

--- main.cpp
+++ main.cpp
@@ -1,29 +1,45 @@
 #include <QtCore/QCoreApplication>
+#include <QStringList>
 #include "portmidi.h"
 #include <iostream>
 #include <QString>
 #include <QDebug>
 #include "Midi_reader.h"
 
-#define MIDI_DEVICE_NAME "YAMAHA"
-
 using namespace std;
 
 int main(int argc, char *argv[]) {
   QCoreApplication a(argc, argv);
+
+  QStringList args = a.arguments();
+  QString deviceName;
+  if (args.size() == 2) {
+    if ((args[1] == "-h") || (args[1] == "--help")) {
+      qDebug("Usage: %s [device_name]", qPrintable(args[0]));
+      return 0;
+    }
+    deviceName = args[1];
+  }
+
   qDebug() << "started!";
   int good_id = -1;
-  int count = Pm_CountDevices();
-  for(int i = 0; i < count; i++) {
-    const PmDeviceInfo* info = Pm_GetDeviceInfo(i);
-    if ( info->input == 1 && QString(info->name).contains(MIDI_DEVICE_NAME)) {
-      good_id = i;
+  if (!deviceName.isNull()) {
+    int count = Pm_CountDevices();
+    for(int i = 0; i < count; i++) {
+      const PmDeviceInfo* info = Pm_GetDeviceInfo(i);
+      if ( info->input == 1 && QString(info->name) == deviceName) {
+        good_id = i;
+      }
+      qDebug() << i << (good_id == i? "(*)":"") << ": " << info->name <<  " input: " << info->input << " output: " << info->output;
     }
-    qDebug() << i << (good_id == i? "(*)":"") << ": " << info->name <<  " input: " << info->input << " output: " << info->output;
   }
   if (good_id < 0) {
-    qDebug() << "Device not found";
-    return 1;
+    if (!deviceName.isNull()) {
+      qDebug() << "Device not found";
+      return 1;
+    } else {
+      good_id = Pm_GetDefaultInputDeviceID();
+    }
   }
   PortMidiStream* s = 0;
   PmError e = Pm_OpenInput(&s, good_id, 0, 100, 0, 0);
